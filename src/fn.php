2f053cfdda68940b17920955faa98458f726fc61
FQ-Programming <linchen@fq-programming.xyz>
2018-08-13 15:06:35 +0200

    12

---
+++ src/fn.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Futurenet | Business 2000</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Michroma|Montserrat" rel="stylesheet"> 
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="assets/css/style.min.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Regna
+    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <a href="index.php"><img src="img/logo1.png" alt="" title="" /></img></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="#hero">Regna</a></h1>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-has-children"><a href="index.php#home">Home</a>
+            <ul>
+              <li><a href="index.php#about">About Business 2000</a></li>
+            </ul>
+          </li>
+          <li class="menu-has-children"><a href="fn.php">Futurenet</a>
+            <ul>
+              <li><a href="fn.php#wifn">Wer ist Futurenet</a></li>
+              <li><a href="fn.php#ulfn">Upgrade Level Futurenet</a></li>
+              <li><a href="fn.php#vgfn">Vergleich Futurenet</a></li>
+              <li><a href="fn.php#spfn">Sport Futurenet</a></li>
+              <li><a href="fn.php#infn">Hilfe Futurenet</a></li>
+            </ul>
+          </li>
+          <li><a href="partners.php">Partner</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Home Section
+  ============================-->
+  <section id="home">
+    <div class="home-container">
+      <h1>Business 2000</h1>
+      <h2>Your Network</h2>
+      <h3>Futurenet</h3>
+    </div>
+  </section><!-- #hero -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="wifn">
+      <div class="container">
+        <div class="row about-container">
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <img src="img/fn.png" alt="FutureNet">
+          </div>
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="title">Was ist Futurenet</h2>
+            <p>
+              Futurenet ist seit 2012 auf dem Markt. Futurenet ist nicht mit anderen Social Media Plattformen vergleichbar. Sowie man Whatsapp nicht mit Skype oder Telegram vergleichbar ist. <br><br>
+              Futurenet ist auch eine Plattform mit offline Einkommen wie Kaffee's und Bars in Polen. In Deutschland werden auch welche er&ouml;ffnet.
+            </p>
+          </div>
+          <div class="col-md-12">
+              - Futurenet ist eine Plattform wo man sich kostenlos anmelden kann <br>
+              - Futurenet ist eine Plattform mit mehr als 3,2 Millionen Mitgliedern Weltweit <br>
+              - Futurenet ist eine Plattform die viele Funktionen beinhaltet <br>
+              - Futurenet ist eine Plattform wo man "machen kann" und "nicht machen muss" <br>
+              - Futurenet ist eine Plattform wo man mit Einsatz von Geld in Werbepakete und deren Werbung die man schaltet Geld verdienen kann <br>
+              - Futurenet ist eine Plattform wo man mit Einsatz in die Matrix CASH BACK erzieht sowie auch Gegenwerte (Einer der Gegenwerte sind z.B. Vorteile durch Upgrade bei Posts auf dem Dashboard) <br>
+              - Futurenet ist eine Plattform die auch im Bereich Social Media verwerdendet werden kann <br>
+              - Futurenet ist eine Plattform wo man auch Spiele spielen kann
+          </div>
+        </div>
+      </div>
+    </section><!-- #about -->
+    <section id="ulfn">
+        <div class="container">
+            <div class="row ulfn-container">
+                <div class="col-md-12">
+                    <h2 class="title text-center">Upgrade Member Level</h2>
+                    <h4 class="title text-center">Als Beispiel bei einem Member im Upgrade Level 100</h4>
+                    <table style="width: 80%;" class="text-center">
+                        <thead>
+                        <tr>
+                        <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                        <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                        <td><em>Maximale Posts im Board pro Stunde</em></td>
+                        <td>10 Posts</td>
+                        </tr>
+                        <tr>
+                        <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                        <td>50 Posts</td>
+                        </tr>
+                        <tr>
+                        <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                        <td>100 Anfragen</td>
+                        </tr>
+                        <tr>
+                        <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                        <td>100 Nachrichten</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <button type="button" class="btn btn-secondary btn-block" data-toggle="modal" data-target="#ugl">
+                            Upgrade Member Level list
+                    </button>
+                    
+                    <!-- Modal -->
+                    <div class="modal fade" id="ugl" tabindex="-1" role="dialog" aria-labelledby="ugl" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="ugl" style="color: #000;">Upgrade Member Level list</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body" style="color: black">
+                        <h3 class="text-center">Free Member</h3>
+                            <table style="width: 80%;">
+                            <thead>
+                            <tr>
+                            <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                            <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <td><em>Maximale Posts im Board pro Stunde</em></td>
+                            <td>10 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                            <td>20 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                            <td>5 Anfragen</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                            <td>5 Nachrichten</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <hr>
+                            <h3 class="text-center">Upgrade 10 Member</h3>
+                            <table style="width: 80%;">
+                            <thead>
+                            <tr>
+                            <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                            <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <td><em>Maximale Posts im Board pro Stunde</em></td>
+                            <td>10 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                            <td>30 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                            <td>10 Anfragen</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                            <td>10 Nachrichten</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <hr>
+                            <h3 class="text-center">Upgrade 25 Member</h3>
+                            <table style="width: 80%;">
+                            <thead>
+                            <tr>
+                            <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                            <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <td><em>Maximale Posts im Board pro Stunde</em></td>
+                            <td>10 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                            <td>30 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                            <td>25 Anfragen</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                            <td>25 Nachrichten</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <hr>
+                            <h3 class="text-center">Upgrade 50 Member</h3>
+                            <table style="width: 80%;">
+                            <thead>
+                            <tr>
+                            <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                            <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <td><em>Maximale Posts im Board pro Stunde</em></td>
+                            <td>10 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                            <td>30 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                            <td>50 Anfragen</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                            <td>50 Nachrichten</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            <hr>
+                            <h3 class="text-center">Upgrade 100 / 500 / 1000 Member</h3>
+                            <table style="width: 80%;">
+                            <thead>
+                            <tr>
+                            <td style="width: 75%;"><strong><u>Info</u></strong></td>
+                            <td style="width: 25%;"><strong><u>Anzahl</u></strong></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                            <td><em>Maximale Posts im Board pro Stunde</em></td>
+                            <td>10 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Posts im Board in 24 Stunden</em></td>
+                            <td>50 Posts</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Freundschaftsanfragen in 24 Stunden</em></td>
+                            <td>100 Anfragen</td>
+                            </tr>
+                            <tr>
+                            <td><em>Maximale Anzahl an Nachrichten an NICHT FREUNDE</em></td>
+                            <td>100 Nachrichten</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary btn-block" data-dismiss="modal">Close</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="vgfn">
+        <div class="container">
+            <div class="row vgfn-container">
+                <div class="col-md-12">
+                    <h2 class="title text-center">
+                        Ist Futurenet vergleichbar mit Facebook oder anderen Social Media Plattformen
+                    </h2>
+                    <p>
+                        Vergleichbar NEIN <br> <br>
+                        Futurenet hat einige Funktionen die auch Facebook und Co. besitzen, ist aber nicht damit vergleichbar. <br>
+                        Denn Futurenet ist einmalig. <br>
+                        Mit Futurenet kann man viele Sachen machen, die man mit den anderen Plattformen nicht kann. <br>
+                        Darunter als Beispiel: Man kann sich als K&uuml;nstler mit seinem K&uuml;nstlernamen registrieren und damit neue Fans erreichen.
+                    </p>
+                    <hr>
+                    <div id="black-content">
+                        <h2 class="title text-center" style="color: white">
+                            Ist Futurenet das richtige f&uuml;r mich?
+                        </h2>
+                        <p>
+                            Da Futurenet einmalig ist sollte man es austesten um entscheiden zu k&ouml;nnen und dies nicht vorschnell machen. Denn wenn man nicht testet wei&szlig; man es nie.
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="spfn">
+        <div class="container">
+            <div class="row spfn-container">
+                <div class="col-md-12">
+                    <h2 class="title text-center">
+                        Infos zu Futurenet Sport
+                    </h2>
+                    <p>
+                        <a href="https://sport.futurenet.club/#sport-is-a-big-part-of-the-futurenet" target="_blank">About Futurenet Sport
+                        </a><br>
+                        <a href="https://sport.futurenet.club/#our-projects" target="_blank">Projects Futurenet Sport
+                        </a><br>
+                        <a href="https://sport.futurenet.club/#our-partners" target="_blank">Partner Futurenet Sport
+                        </a><br>
+                        <a href="https://sport.futurenet.club/#upcomming-events" target="_blank">Events Futurenet Sport
+                        </a><br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>Infos</h3>
+          </div>
+          <div class="col-md-4">
+            <h3>Partner</h3>
+          </div>
+          <div class="col-md-4">
+            <h3>Rechtliches</h3>
+            <a href="impressum.php">Impressum</a><br>
+            <a href="datenschutz.php">Datenschutz</a><br>
+            <a href="contact.php">Kontakt</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright by <strong>Business 2000</strong> | Created by <strong>FQ-Programming</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Regna
+        -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="assets/js/global.min.js"></script>
+
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home | Business 2000</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Michroma|Montserrat" rel="stylesheet"> 
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="assets/css/style.min.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Regna
+    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <a href="index.php"><img src="img/logo1.png" alt="" title="" /></img></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="#hero">Regna</a></h1>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-has-children"><a href="index.php#home">Home</a>
+            <ul>
+              <li><a href="index.php#about">About Business 2000</a></li>
+            </ul>
+          </li>
+          <li class="menu-has-children"><a href="fn.php">Futurenet</a>
+            <ul>
+              <li><a href="fn.php#wifn">Wer ist Futurenet</a></li>
+              <li><a href="fn.php#ulfn">Upgrade Level Futurenet</a></li>
+              <li><a href="fn.php#vgfn">Vergleich Futurenet</a></li>
+              <li><a href="fn.php#spfn">Sport Futurenet</a></li>
+              <li><a href="fn.php#infn">Hilfe Futurenet</a></li>
+            </ul>
+          </li>
+          <li><a href="partners.php">Partner</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Home Section
+  ============================-->
+  <section id="home">
+    <div class="home-container">
+      <h1>Business 2000</h1>
+      <h2>Your Network</h2>
+      <h3>Startseite</h3>
+    </div>
+  </section><!-- #hero -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-12 content order-lg-1 order-2">
+            <h2 class="title">Was ist Business 2000</h2>
+            <p>
+              Business 2000 ist ein Projekt von mehreren Menschen die mit einsetzen von Geld ein Cash Back erziehlen m&ouml;chten. <br>
+              Dabei gilt das oberste Gebot in diesem Bereich. <br>
+              <h4 style="color: red; text-transform: uppercase">wir bieten keine Investion, sondern bieten eine m&ouml;glichkeit mit einem einsatz an Geld Cash Back zu erziehlen. <br><br>
+              Desweiteren ist bei uns auch Gebot: "Setze nur das ein was du auch &uuml;brig hasst und nicht verhungern musst".</h2>
+            </p>
+
+          </div>
+
+          
+        </div>
+
+      </div>
+    </section><!-- #about -->
+    <section id="fn-vorab">
+      <div class="container">
+        <div class="row fn-vorab-container">
+          <div class="col-md-9 content order-md-1 order-2">
+            <h2 class="title">Vorab Informationen &uuml;ber Futurenet</h2>
+            <p>
+              Hier finden Sie einige Vorab Informationen die Sie sich anschauen sollten. <br><br>
+              <a href="https://www.youtube.com/watch?v=S9N33Hk117U&feature=youtu.be" target="_blank"><button class="btn btn-info btn-block">Youtube Pr&auml;stentation</button></a><br><br>
+              Sie m&ouml;chten weitere Infos &uuml;ber Futurenet und was wir dazusagen, klicken Sie entweder auf den nachstehenden Button oder &uuml;ber die Navigation <br>
+              <a href="/fn.php"><button class="btn btn-info btn-block">Infos &amp; erfahrungen</button></a>
+            </p>
+          </div>
+          <div class="col-md-3 content order-md-1 order-2">
+          <img src="//qr.b2000.xyz/M_Weigand_FN.png" style="height: 350px;width:269px;" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--==========================
+      Facts Section
+    ============================-->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>Infos</h3>
+          </div>
+          <div class="col-md-4">
+            <h3>Partner</h3>
+          </div>
+          <div class="col-md-4">
+            <h3>Rechtliches</h3>
+            <a href="impressum.php">Impressum</a><br>
+            <a href="datenschutz.php">Datenschutz</a><br>
+            <a href="contact.php">Kontakt</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright by <strong>Business 2000</strong> | Created by <strong>FQ-Programming</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Regna
+        -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script>
+
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="assets/js/global.min.js"></script>
+
+</body>
+</html>
